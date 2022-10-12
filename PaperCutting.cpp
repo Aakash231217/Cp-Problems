c1cd1c9acea2e0332b140d2ccb8f1f8a1754a498
joker_007 <92391710+Aakash231217@users.noreply.github.com>
2022-10-13 01:55:18 +0530

    PaperCutting.cpp
    A codechef Contest solution

---
+++ PaperCutting.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    int temp;
+	    if(n>=k)temp=n/k;
+	    cout<<temp*temp<<endl;
+	    
+	}
+	return 0;
+}
