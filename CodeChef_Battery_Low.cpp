3614ef369f625786bf9133abd2b75c71e52d1844
SAHIL GUPTA <41647125+sahilgupta1462@users.noreply.github.com>
2022-10-13 16:02:38 +0530

    Create CodeChef_Battery_Low.cpp

---
+++ CodeChef_Battery_Low.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int Length,b;
+	cin>>Length;
+	for(int i=0;i<Length;i++){
+	   cin>>b;
+	   b<=15?cout<<"Yes\n":cout<<"No\n";
+	}
+	return 0;
+	
+}
