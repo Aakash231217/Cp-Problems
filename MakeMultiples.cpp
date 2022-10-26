dba2db4b05fddc4e92ca5b9da7d353eb6634464f
joker_007 <92391710+Aakash231217@users.noreply.github.com>
2022-10-26 23:37:11 +0530

    Create MakeMultiples.cpp

---
+++ MakeMultiples.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(b%a==0)cout<<"YES"<<endl;
+	    else if(b>2*a)cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+	return 0;
+}
